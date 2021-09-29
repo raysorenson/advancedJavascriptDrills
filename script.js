b4632617a3609f576d2d3b066aca0dae74c44754
Ray Sorenson <stngry78@gmail.com>
2021-09-29 18:56:11 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// name = 'Covalence';
+// var name;
+// console.log(name);
+
+
+// setName();
+// function setName() {
+//     var name = 'Covalence';
+//     console.log(name);
+// }
+
+// console.log('some text1');
+// let avg = findAvg(2, 2);
+// console.log('some text2', avg);
+// function findAvg(a, b) {
+//     console.log('some text3');
+//     var answer = (a + b) / 2;
+//     return answer;
+// }
+
+let fruits = ['apple', 'tomato', 'banana'];
+//let favFruit;
+function printFruits() {
+    let favFruit = fruits[2];
+  //  favFruit = fruits[2];
+    console.log(fruits[0]);
+}
+
+function printFavFruit() {
+    console.log(favFruit);
+}
+
+printFruits();
+printFavFruit();
